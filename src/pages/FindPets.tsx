8588f5f89aecc02e36e91430665e6ff19e3de7ad
tommyrharper <thomasrobertharper@gmail.com>
2021-06-08 03:25:02 +0100

    Clean up FindPets page

--- src/pages/FindPets.tsx
+++ src/pages/FindPets.tsx
@@ -1,92 +1,31 @@
-import React, {useState} from 'react'
+import React from 'react'
 // import gql from 'graphql-tag'
 import { gql } from 'graphql-request'
-// import { useQuery, useMutation } from '@apollo/react-hooks'
-import { useQuery, useMutation } from 'atomiql'
-// import { useQuery } from 'atomiql'
+import { useQuery } from 'atomiql'
 import PetsList from '../components/PetsList'
-import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
-import { Pet } from '../components/NewPet'
 import { Component3 } from '../components/Component3'
 
-const PETS_FIELDS = gql`
-  fragment PetsFields on Pet {
-    id
+
+export const GET_PET = gql`
+query {
+  pet(id: "2l3krjhwelkfhaiewua") {
     name
-    type
+    id
+    createdAt
     img
-    # vaccinated @client
-    owner {
-      id
-      # age @client
-    }
   }
+}
 `
-
-export const GET_PETS = gql`
-  query GetPets {
-    pets {
-      ...PetsFields
-    }
-  }
-  ${PETS_FIELDS}
-`
-
-const ADD_PET = gql`
-  mutation AddPet($input: NewPetInput!) {
-    addPet(input: $input) {
-      ...PetsFields
-    }
-  }
-  ${PETS_FIELDS}
-`
-
-// const examplePet: Pet = {
-//   img: 'asdfasdf',
-//   id: 'asdfasdf',
-//   name: 'Tom',
-//   type: 'CAT'
-// }
-
 
 
 export default function FindPets () {
-  // const { data, loading, error } = useQuery(GET_PETS)
-  const [modal, setModal] = useState(false)
-  const [ data, loading, error ] = useQuery(GET_PETS)
-  const [addPet, newPet] = useMutation(
-    ADD_PET,
-    (cache: any, { data: { addPet } }: any) => {
-      const { data: { pets }, writeAtom } = cache.readQuery(GET_PETS);
-      writeAtom({
-        pets: [addPet, ...pets]
-      })
-    },
-  //   // GET_PETS
-  //   // {
-  //   //   update(cache, { data: { addPet } }) {
-  //   //     const { pets } = cache.readQuery({ query: GET_PETS })
-  //   //     cache.writeQuery({
-  //   //       query: GET_PETS,
-  //   //       data: { pets: [addPet, ...pets] }
-  //   //     })
-  //   //   }
-  //   // }
-  )
-
-  const onSubmit = (input: Pet) => {
-    addPet({input})
-    setModal(false)
-  }
+  const [ data, loading, error ] = useQuery(GET_PET)
+  console.log(`data in FindPets: `, data);
 
   if (loading) return <Loader />
 
-  if (error || newPet.hasError) return <span>Error</span>
-  
-  if (modal) {
-    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
-  }
+  if (error) return <span>Error</span>
 
   return (
     <div className="page pets-page">
@@ -96,14 +35,10 @@ export default function FindPets () {
           <div className="col-xs-10">
             <h1>Component 1</h1>
           </div>
-
-          <div className="col-xs-2">
-            <button onClick={() => setModal(true)}>new pet</button>
-          </div>
         </div>
       </section>
       <section>
-        { !loading && !error && <PetsList pets={data?.pets} /> }
+        { !loading && !error && <PetsList pets={[data?.pet]} /> }
       </section>
       <Component3 />
     </div>
