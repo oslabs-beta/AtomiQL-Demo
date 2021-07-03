bf2dcad10c5fb9a7a167b6ef5d3e0056f484d3aa
tommyrharper <thomasrobertharper@gmail.com>
2021-07-03 20:37:41 +0100

    Update resolvers

--- src/AtomiClient/resolvers.ts
+++ src/AtomiClient/resolvers.ts
@@ -1,7 +1,17 @@
 export const resolvers = {
+  Query: {
+    pets() {
+      return 'asdfas'
+    },
+  },
   Pet: {
     name() {
       return 'Jones Local'
     },
   },
+  User: {
+    id() {
+      return 'local-id'
+    }
+  }
 }
